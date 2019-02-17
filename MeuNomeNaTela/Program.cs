da5d2cf08672cf1f928576660b8bd8fa85983f37
Rondinele Guimaraes <ron-dinele@hotmail.com>
2019-02-16 23:17:04 -0200

    Ajustando o codigo

---
+++ MeuNomeNaTela/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MeuNomeNaTela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string meuNome;     
+            Console.Title = "Guia Csharp";            
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Qual e o seu nome : ");
+            meuNome = Console.ReadLine();
+            Console.WriteLine(string.Format("Meu nome é: {0}", meuNome));
+            Console.WriteLine("Meu nome é: " + meuNome);
+            Console.Write(meuNome);
+            Console.ReadKey();
+
+
+        }
+    }
+}
