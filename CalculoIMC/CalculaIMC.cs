3057b22d4086c4197310abdf02ed8c2e52aa2c8d
Rondinele Guimaraes <ron-dinele@hotmail.com>
2019-02-16 23:06:46 -0300

    Criando projeto de calculo de IMC

---
+++ CalculoIMC/CalculaIMC.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CalculoIMC
+{
+    internal class CalculaIMC
+    {                            
+        public Double Calcular(Double peso, Double altura)
+        {
+            return  ((peso) / (altura * altura));           
+        }
+        
+        public String ClassificarIMC(Double imc)
+        {
+            if (imc < 16)
+            {
+                return "Magreza grave";
+            }
+            else if (imc >= 16 && imc < 17)
+            {
+                return "Magreza moderada";
+            }
+            else if (imc >= 17 && imc < 18.5)
+            {
+                return "Magreza leve";
+            }
+            else if (imc >= 18.5 && imc < 25)
+            {
+                return "Saudável";
+            }
+            else if (imc >= 25 && imc < 30)
+            {
+                return "Sobrepeso";
+            }
+            else if (imc >= 30 && imc < 35)
+            {
+                return "Obesidade Grau 1";
+            }
+            else if (imc >= 35 && imc < 40)
+            {
+                return "Obesidade Grau 2 (severa)";
+            }
+            else if (imc > 40)
+            {
+                return "Obesidade Grau 3 (mórbida)";
+            }
+            return "";
+
+        }
+                          
+    }
+
+}
---
+++ CalculoIMC/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CalculoIMC
+{
+    internal class Program
+    {
+        private static void Main(String[] args)
+        {
+            double peso = 0, altura = 0, valorIMC = 0;
+
+            Console.WriteLine("Digite o valor da massa corporal em Kg :");
+
+            peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da altura em Metros utilizando Virgula !!: ");
+
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            var calculaIMC = new CalculaIMC();
+
+            valorIMC = calculaIMC.Calcular(peso, altura);
+
+            Console.WriteLine("Resultado do IMC é : " + valorIMC);
+
+            Console.WriteLine("Classificação : " + calculaIMC.ClassificarIMC(valorIMC));
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
