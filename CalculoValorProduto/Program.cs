96f222aefc9df969a392694d1a7d55c095e23dcd
Rondinele Guimaraes <rondineleg@edu.univai.br>
2019-04-29 10:54:54 -0300

    Algoritmo Calculo de Produto

---
+++ CalculoValorProduto/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace CalculoValorProduto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              
+            string nomeProduto = "";      
+            int quantidade = 0;    
+            double preco = 0;
+            double percentualDesconto = 0;
+            double totalDesconto = 0;
+            double totalBruto = 0;
+            
+            Console.Write("Entre com o nome do Produto : ");     
+            nomeProduto = Console.ReadLine();                           
+
+            Console.Write("Entre com a quantidade : ");
+            quantidade = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com preço do produto : ");
+            preco = double.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o percentual de desconto : ");
+            percentualDesconto = double.Parse(Console.ReadLine());
+
+            totalBruto = quantidade * preco;             
+            totalDesconto = totalBruto - ((percentualDesconto / 100) * totalBruto);
+            Console.Write("Total com desconto : " + totalDesconto);
+            Console.ReadKey();  
+        }
+    }
+}
