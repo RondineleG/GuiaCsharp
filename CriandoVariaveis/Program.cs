7e9221f473dda9c792fd5f40f04565b26a9298af
Rondinele Guimaraes <rondineleg@edu.univai.br>
2019-07-01 11:07:09 -0300

    Criando variaveis

---
+++ CriandoVariaveis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Executando projeto - Criando Variaveis");
+
+            int idade;
+            string nome = "Rondinele";
+            idade = 27; 
+            Console.WriteLine("Ola " + nome + ": Sua idade é " + idade +  "!");
+            Console.WriteLine("Execução finalizada. Tecle enter para sair. . . ");
+            Console.ReadLine();
+        }
+    }
+}
