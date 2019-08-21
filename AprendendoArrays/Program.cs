bda3c1206e726a690201be0458644f51bf6363bd
Rondinele Guimaraes <rondineleg@edu.univai.br>
2019-08-21 08:12:53 -0300

    criando arrays usando var

--- AprendendoArrays/Program.cs
+++ AprendendoArrays/Program.cs
@@ -5,6 +5,11 @@ namespace AprendendoArrays
     {
         static void Main()
         {
+            Console.Title = "Arrays";
+            // criando array usando var
+            var novoArray = new int[2];
+            novoArray[0] = 10;
+            novoArray[1] = 2;
             //Criando Array e definindo valores
             int[] array = new int[] { 1, 2, 3, 4, 5 };
             //Lendo  valor de array na posicao 4
@@ -15,6 +20,8 @@ namespace AprendendoArrays
             array2[0] = 50;
             //Lendo  valor de array na posicao 0
             Console.WriteLine(array2[0]);
+            Console.WriteLine(novoArray[1]);
+
             Console.ReadKey();
         }
     }
