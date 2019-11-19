61f9e961af1a8ca73853e4823d903a39544fb887
Rondinele <rondineleg@edu.univali.br>
2019-11-19 12:18:39 -0300

    criando objeto do tipo anônimo

---
+++ TiposAnonimos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TiposAnonimos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tipoAnonimo = new
+            {
+                PrimeiroNome = "Rondinele",
+                SegundoNome = "Sousa",
+                Idade = 27,
+                Vivo = true
+            };
+
+            Console.WriteLine(tipoAnonimo.Idade);
+        }
+    }
+}
