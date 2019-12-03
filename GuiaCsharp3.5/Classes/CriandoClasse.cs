fa1eca1e08153dc9540635245f63e3bcbafb8cb2
Rondiele Guimaraes <rondineleg@edu.univali.br>
2019-12-03 11:34:59 -0200

    Criando classes e propriedades

---
+++ GuiaCsharp3.5/Classes/CriandoClasse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classes
+{
+    public class CriandoClasse
+    {
+        public string  campo = string.Empty;    
+
+    public void MeuMetodo(int primeiroParametro, string segundoParametro)
+    {
+        Console.WriteLine("Primeiro Parametro {0}, Segundo Parametro {1}",
+                                                    primeiroParametro, segundoParametro);
+    }
+
+    public int PropriedadeAutomatica { get; set; }
+
+
+    private int _minhaPropriedade;
+
+    public int MinhaPropriedade
+    {
+        get { return _minhaPropriedade; }
+        set { _minhaPropriedade = value; }
+    } 
+    }
+
+}
---
+++ GuiaCsharp3.5/Keywords/Keywords.cs
@@ -0,0 +1,23 @@
+﻿namespace Keywords
+{
+    public class Keywords
+    {
+        public Keywords() { }
+
+        public int PropriedadePublica { get; set; }
+
+        private int PropriedadePrivada
+        {
+            private get;
+            private set;
+
+        }
+
+        struct CriandoStruct
+        {
+            public decimal preco;
+            public string nome;
+            public string descricao;
+        }
+    }
+}
