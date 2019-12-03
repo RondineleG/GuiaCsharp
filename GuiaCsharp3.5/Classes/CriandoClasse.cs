f5f963184381d14801b0c6a71e9961cb46268fca
Rondiele Guimaraes <rondineleg@edu.univali.br>
2019-12-03 11:40:32 -0200

    aplicar lógica de adição em get e set

--- GuiaCsharp3.5/Classes/CriandoClasse.cs
+++ GuiaCsharp3.5/Classes/CriandoClasse.cs
@@ -3,38 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Classes
-{
+namespace Classes {
     // Modificadores de acesso publico
-    public class CriandoClasse
-    {
+    public class CriandoClasse {
         //propriedade
-        public string  campo = string.Empty;
+        public string campo = string.Empty;
 
         //Contrutor
-        public CriandoClasse()
-        {
+        public CriandoClasse () {
 
         }
         //metodo
-    public void MeuMetodo(int primeiroParametro, string segundoParametro)
-    {
-        Console.WriteLine("Primeiro Parametro {0}, Segundo Parametro {1}",
-                                                    primeiroParametro, segundoParametro);
-    }
+        public void MeuMetodo (int primeiroParametro, string segundoParametro) {
+            Console.WriteLine ("Primeiro Parametro {0}, Segundo Parametro {1}",
+                primeiroParametro, segundoParametro);
+        }
 
         //propriedade automatica
-    public int PropriedadeAutomatica { get; set; }
-
+        public int PropriedadeAutomatica { get; set; }
 
         //propriedade encapsulada
-    private int _minhaPropriedade;
+        private int _minhaPropriedade;
 
-    public int MinhaPropriedade
-    {
-        get { return _minhaPropriedade; }
-        set { _minhaPropriedade = value; }
-    } 
+        public int MinhaPropriedade {
+            get { return _minhaPropriedade; }
+            set { _minhaPropriedade = value; }
+        }
+
+        private int _aplicandoLogica;
+
+        public int Logica {
+            get {
+                return _aplicandoLogica / 2;
+            }
+
+            set {
+                if (value > 100)
+                    _aplicandoLogica = 100;
+                else
+                    _aplicandoLogica = value;;
+            }
+        }
     }
-
 }
