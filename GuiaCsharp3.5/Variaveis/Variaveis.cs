a830fbaf75ca07ce0bb32bcb292466d5179f0a42
Rondiele Guimaraes <rondineleg@edu.univali.br>
2019-12-03 11:07:43 -0200

    Projeto usando .net 3.5

---
+++ GuiaCsharp3.5/Variaveis/Variaveis.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Variaveis
+{ 
+
+
+    class Program
+    {       
+
+        static void Main()
+        {
+            int idade;
+            double peso;
+            float altura;
+            string nome;
+            char sexo;
+            decimal preco;
+            Console.Title = "Variaveis";
+            
+            Console.WriteLine("Qual seu nome ?");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Qual sua idade?");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+
+            if (idade < 18)
+            {
+                Console.WriteLine("Idade nao permitida");
+                
+            }
+            else
+            {
+                Console.WriteLine("Ola " + nome + "com " + idade + " anos ja e um adulto");
+            }
+                Console.ReadKey();
+        }
+    }
+}
